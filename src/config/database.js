6675c69ff83ebbfe129f0b38348dabf4b61faf06
Pedro Paulo Machado S. Souza <47438558+discrete-Mathematics@users.noreply.github.com>
2020-04-30 23:23:04 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ module.exports = {
     dialect: 'mysql',
     host: 'localhost',
     username: 'root',
-    password: '#PedroPaulo1309',
+    password: '',
     database: 'clientes',
     define: {
         timestamps: true,  //created_at && updated_at
